219b2bc10a1d6d05e87e1c7cfe8510881d4fdaa5
Leandro Amaral <zehamarall@gmail.com>
2016-05-13 00:33:29 -0300

    adicionando a execução de testes au rodar o docker e organizando o codigo

--- server/server.js
+++ server/server.js
@@ -3,6 +3,7 @@
 var express = require('express');
 var app = express();
 var bodyParser = require('body-parser');
+var palindrome = require('./palindrome');
 
 app.use(express.static(__dirname + '/public'));
 app.use(bodyParser.json());
@@ -20,25 +21,11 @@ app.get('/', function(req, res) {
 	res.json(true);
 });
 
-
-function isPalindrome (str){
-	var streplace = str.replace(/[^A-Za-z0-9]/g, '')
-	var reversestr = str.split('').reverse().join('');
-				
-	if (streplace.toUpperCase() === reversestr.toUpperCase()){
-		console.log("E palindrome  ", streplace, reversestr)
-		return true	
-	} else {
-		console.log("Nao E palindrome  ", streplace, reversestr)
-		return false
-	}
-}
-
 app.post('/palindrome', function(req, res) {
 		
 	var strrec = req.body.palindrome
 		
-	if (isPalindrome(strrec)){
+	if (palindrome.isPalindrome(strrec)){
 		res.json(true)
 	}else{
 		res.status(400).send('Current string isnt palindrome')
